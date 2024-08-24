0ef713659fd0ae7da5f3292a9cd09d50016ca0e9
bettatested <123904088+bettatested@users.noreply.github.com>
2024-08-24 01:48:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function addSuspense() { // Plays suspense
+    const suspense = new Audio('suspense.mp3');
+    suspense.play()
+}
+
+function addCat() { // Plays cat
+    const cat = new Audio('cat.mp3');
+    cat.play()
+}
+
+function addDuck() { // Plays duck
+    const duck = new Audio('duck.mp3');
+    duck.play()
+}
